bc76ca2b6e83c7ad7605935a9109d62563fa7092
nalutm <nluizatm@gmail.com>
2024-12-30 23:04:02 -0300

    :sparkles: feat: Express.js assignment 01

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ My solutions to assignments from the [Node.js course by Maximilian Schwarzmülle
    - Return a list of dummy users (e.g. `<ul><li>User1</li></ul>`)
 3. Add a form with a "username" `<inpu>`to the "/" page and submit a POST request to "/create-user" upon a button click
 4. Add the "/create-user" route and parse the incoming data (i.e. the username) and simply log it to the console
+
+## Express assignment01
+
+1. Create a npm project and install Express.js (Nodemon if you want)
+2. Create an Express.js app which funnels the requests through 2 middleware functions that log something to the console and return one response
+3. Handle requests to "/" and "/users" such that each request only has one handler/middleware that does something with it (e.g. send dummy response)
---
+++ express.js/assignment01/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+// Create an Express.js app which funnels the requests through 2 middleware functions that log something to the console and return one response∂
+// app.use((req, res, next) => {
+//     console.log("Here is a middleware!");
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log("Here is another middleware!");
+//     res.send("<h1>Express.js assignment 01 (: </h1>");
+// });
+
+// Handle requests to "/" and "/users" such that each request only has one handler/middleware that does something with it (e.g. send dummy response)
+app.use('/users', (req, res, next) => {
+    res.send('<h1>The "Users" page (:</h1>')
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Working on Express.js assignment 01! (: </h1>')
+});
+
+app.listen(3000);
